66bcf3a9900759c3e6463ebed88b6b3a2ea2b45d
kiu_maghlakelidze.saba <maghlakelidze.saba@kiu.edu.ge>
2023-02-11 20:44:57 +0400

    routing with id

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,12 +1,18 @@
+import { Link } from "react-router-dom";
+
 const BlogList = ({blogs, title}) => {
     return (
         <div className="blog-list">
             <h2>{title}</h2>
             {blogs.map(blog => (
+                    <Link to={`/blogs/${blog.id}`}>
+
                 <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
+                        <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
                 </div>
+                </Link>
+
             ))}
         </div>
     );
