1c700f1a64a2a8c6c0dafca07fc8911e37d2424e
kiu_maghlakelidze.saba <maghlakelidze.saba@kiu.edu.ge>
2023-02-12 19:13:26 +0400

    updated blog-details

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,12 +1,23 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch"
 
 const BlogDetails = () => {
     //id is the name of the parameter in route /blogs/:id
     const { id } = useParams()
+    const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id)
 
     return (
         <div className="blog-details">
-            <h2>Blog details - {id} </h2>
+            { isPending && <div>Loading...</div>}
+            { error && <div>{error}</div>}
+            { blog && (
+                <article>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                    <div>{blog.body}</div>
+                </article>
+            )}
+            {/* <h2>Blog details - {id} </h2> */}
         </div>
     );
 }
