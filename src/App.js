fbcd7260ce2762f0eb58f3c2783b6840e6ef359f
kiu_maghlakelidze.saba <maghlakelidze.saba@kiu.edu.ge>
2023-02-12 20:43:23 +0400

    complete API

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './Home';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Create from './Create';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
   return (
@@ -21,6 +22,9 @@ function App() {
             {/* id is a parameter, we define a param with : */}
             <Route path="/blogs/:id">
               <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
             </Route>
           </Switch>
         </div>
--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,19 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import useFetch from "./useFetch"
 
 const BlogDetails = () => {
     //id is the name of the parameter in route /blogs/:id
     const { id } = useParams()
     const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id)
+    const timeAndSpace = useHistory()
+
+    const hanldeClick = () => {
+        fetch('http://localhost:8000/blogs/' + blog.id, {
+            method: 'DELETE'
+        }).then(() => {
+            timeAndSpace.push('/')
+        }) 
+    }
 
     return (
         <div className="blog-details">
@@ -15,6 +24,7 @@ const BlogDetails = () => {
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
                     <div>{blog.body}</div>
+                    <button onClick={hanldeClick}>delete</button>
                 </article>
             )}
             {/* <h2>Blog details - {id} </h2> */}
